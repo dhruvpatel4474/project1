91ad22aacadc56a31ff18c66ae0882752de0d1e9
raj159557 <37499990+raj159557@users.noreply.github.com>
2018-03-24 19:14:38 +0530

    Image Add

--- app/src/main/java/com/geekcoders/payingguest/Activities/AddPgActivity.java
+++ app/src/main/java/com/geekcoders/payingguest/Activities/AddPgActivity.java
@@ -1,13 +1,23 @@
 package com.geekcoders.payingguest.Activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -24,22 +34,26 @@ import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AddPgActivity extends AppCompatActivity {
+public class AddPgActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText edtTitle;
-    EditText edtPrice,edtNumber,edtAddress,edtDescription;
+    EditText edtPrice, edtNumber, edtAddress, edtDescription;
     Spinner spinnerCity;
     Spinner spinnerCategory;
     Button btnAddPG;
     private ParseObject objPGParse;
     private Bitmap imageBit;
     private ParseFile parseFile;
+    private ImageView imgPG;
+    private int PICK_IMAGE_REQUEST = 1;
+    private String TAG = "Permission";
 
 
     @Override
@@ -50,34 +64,25 @@ public class AddPgActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         Parse.initialize(AddPgActivity.this);
         Constant.mcontext = AddPgActivity.this;
-
-        btnAddPG.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (validate())
-                {
-                    CreatePostOnServer();
-                }
-            }
-        });
     }
 
 
-    public void CreatePostOnServer() {
+    public void CreatePostOnServer() throws Exception {
         ParseACL acl = new ParseACL();
         acl.setPublicReadAccess(true);
         acl.setPublicWriteAccess(true);
         objPGParse = new ParseObject("PGDetail");
         objPGParse.put("title", edtTitle.getText().toString());
         objPGParse.put("price", edtPrice.getText().toString());// int
-        objPGParse.put("city", spinnerCity.getSelectedItem().toString());
+        Category city = (Category) spinnerCity.getSelectedItem();
+        objPGParse.put("city", city.getName().toString());
         objPGParse.put("description", edtDescription.getText().toString());
         objPGParse.put("userId", Constant.getValueForKeyString("userId"));
         objPGParse.put("address", edtAddress.getText().toString());
         objPGParse.put("number", edtNumber.getText().toString());
         objPGParse.put("userName", Constant.getValueForKeyString("name"));
         Category category = (Category) spinnerCategory.getSelectedItem();
-        objPGParse.put("categoryId",category.getObjectId());
+        objPGParse.put("categoryId", category.getObjectId().toString());
 
         objPGParse.setACL(acl);
         objPGParse.saveInBackground((new SaveCallback() {
@@ -170,7 +175,7 @@ public class AddPgActivity extends AppCompatActivity {
                         catList.add(obj);
                     }
                     // returnList(catList);
-                    ArrayAdapter arrayAdapter = new ArrayAdapter(AddPgActivity.this,android.R.layout.simple_list_item_1,catList);
+                    ArrayAdapter arrayAdapter = new ArrayAdapter(AddPgActivity.this, android.R.layout.simple_list_item_1, catList);
                     spinnerCategory.setAdapter(arrayAdapter);
 
 
@@ -199,7 +204,7 @@ public class AddPgActivity extends AppCompatActivity {
                         obj.setObjectId(objectId);
                         catList.add(obj);
                     }
-                    ArrayAdapter arrayAdapter = new ArrayAdapter(AddPgActivity.this,android.R.layout.simple_list_item_1,catList);
+                    ArrayAdapter arrayAdapter = new ArrayAdapter(AddPgActivity.this, android.R.layout.simple_list_item_1, catList);
                     spinnerCity.setAdapter(arrayAdapter);
 
                 }
@@ -210,45 +215,119 @@ public class AddPgActivity extends AppCompatActivity {
     }
 
     public boolean validate() {
-        if (edtTitle.getText().toString().trim().equals(""))
-        {
+        if (edtTitle.getText().toString().trim().equals("")) {
             edtTitle.setError("Please Enter Title");
             return false;
-        }
-        else if (edtAddress.getText().toString().trim().equals("")){
+        } else if (edtAddress.getText().toString().trim().equals("")) {
             edtAddress.setError("Please Enter Address");
             return false;
-        }else if (edtDescription.getText().toString().trim().equals(""))
-        {
+        } else if (edtDescription.getText().toString().trim().equals("")) {
             edtDescription.setError("Please Enter Description");
             return false;
-        }else if (edtNumber.getText().toString().trim().equals(""))
-        {
+        } else if (edtNumber.getText().toString().trim().equals("")) {
             edtNumber.setError("Please Enter Number");
             return false;
-        }
-        else if (edtPrice.getText().toString().trim().equals(""))
-        {
+        } else if (edtPrice.getText().toString().trim().equals("")) {
             edtPrice.setError("Please Enter Price");
             return false;
-        }else {
+        } else {
             return true;
         }
     }
 
-    public void initialize()
-    {
+    public void initialize() {
         edtTitle = (EditText) findViewById(R.id.edt_title);
-        edtPrice = (EditText)findViewById(R.id.edt_price);
-        edtNumber = (EditText)findViewById(R.id.edtNumber);
-        edtAddress = (EditText)findViewById(R.id.edt_address);
-        edtDescription = (EditText)findViewById(R.id.edt_description);
-        spinnerCity = (Spinner)findViewById(R.id.spinner_city);
-        spinnerCategory = (Spinner)findViewById(R.id.spinner_category);
-        btnAddPG = (Button)findViewById(R.id.btnAddPG);
+        edtPrice = (EditText) findViewById(R.id.edt_price);
+        edtNumber = (EditText) findViewById(R.id.edtNumber);
+        edtAddress = (EditText) findViewById(R.id.edt_address);
+        edtDescription = (EditText) findViewById(R.id.edt_description);
+        spinnerCity = (Spinner) findViewById(R.id.spinner_city);
+        spinnerCategory = (Spinner) findViewById(R.id.spinner_category);
+        btnAddPG = (Button) findViewById(R.id.btnAddPG);
+        imgPG = (ImageView) findViewById(R.id.img_adddpg);
 
+        btnAddPG.setOnClickListener(this);
+        imgPG.setOnClickListener(this);
         CategoryList();
         CityList();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnAddPG:
+                if (validate()) {
+                    try {
+                        CreatePostOnServer();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+
+            case R.id.img_adddpg:
+                readImage();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+                imgPG.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void readImage()
+    {
+        if (isStoragePermissionGranted())
+        {
+            Intent intent = new Intent();
+            // Show only images, no videos or anything else
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            // Always show the chooser (if there are multiple options available)
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+        }
+    }
+
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //resume tasks needing this permission
+            readImage();
+        }
     }
 }
 
