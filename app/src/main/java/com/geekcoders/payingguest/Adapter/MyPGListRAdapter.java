a3be8e7f937ef4ebb6a3c89758a5246f1d4a07bf
raj159557 <37499990+raj159557@users.noreply.github.com>
2018-03-25 18:33:05 +0530

    kk

--- app/src/main/java/com/geekcoders/payingguest/Adapter/MyPGListRAdapter.java
+++ app/src/main/java/com/geekcoders/payingguest/Adapter/MyPGListRAdapter.java
@@ -2,14 +2,21 @@ package com.geekcoders.payingguest.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
+import com.daimajia.swipe.implments.SwipeItemRecyclerMangerImpl;
+import com.daimajia.swipe.util.Attributes;
 import com.geekcoders.payingguest.Activities.PgDetailActivity;
 import com.geekcoders.payingguest.Objects.PGObject;
 import com.geekcoders.payingguest.R;
@@ -27,10 +34,15 @@ public class MyPGListRAdapter extends RecyclerSwipeAdapter<MyPGListRAdapter.MyVi
 
     private final Context context;
     private final ArrayList<PGObject> arrayList;
+    SwipeLayout mItemManger;
+    SwipeItemRecyclerMangerImpl recyclerManger;
 
     public MyPGListRAdapter(Context context, ArrayList<PGObject> arrayList) {
         this.context = context;
         this.arrayList = arrayList;
+        mItemManger = new SwipeLayout(context);
+        recyclerManger = new SwipeItemRecyclerMangerImpl(this);
+        setMode(Attributes.Mode.Single);
     }
 
     @Override
@@ -41,7 +53,7 @@ public class MyPGListRAdapter extends RecyclerSwipeAdapter<MyPGListRAdapter.MyVi
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, final int position) {
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
         holder.name.setText(arrayList.get(position).getName());
         Picasso.get()
                 .load(arrayList.get(position).getImage())
@@ -65,6 +77,73 @@ public class MyPGListRAdapter extends RecyclerSwipeAdapter<MyPGListRAdapter.MyVi
                 context.startActivity(intent);
             }
         });
+
+        holder.swipeLayout.setShowMode(SwipeLayout.ShowMode.PullOut);
+        holder.swipeLayout.addDrag(SwipeLayout.DragEdge.Left, null);
+        holder.swipeLayout.addDrag(SwipeLayout.DragEdge.Right, holder.itemView.findViewById(R.id.bottom_wrapper));
+        //holder.swipeLayout.addDrag(SwipeLayout.DragEdge.Right, holder.itemView.findViewById(R.id.second//_wrapper));
+        holder.swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(final SwipeLayout layout) {
+                //when the BottomView totally show.
+                holder.lineLayDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //objectProductArrayList.remove(position);
+
+                        Toast.makeText(context,"Hello",Toast.LENGTH_LONG).show();
+
+                        layout.close();
+                       /* Snackbar snackbar = Snackbar
+                                .make(context, Html.fromHtml("<font color=\"#ffffff\">Item is deleted</font>"), Snackbar.LENGTH_LONG)
+                                .setAction("UNDO", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Snackbar snackbar1 = Snackbar.make(context.getActivity().findViewById(android.R.id.content), Html.fromHtml("<font color=\"#ffffff\">Item is restored!</font>"), Snackbar.LENGTH_SHORT);
+                                        objectProductArrayList.add(position, object);
+                                        snackbar1.setActionTextColor(context.getResources().getColor(R.color.colorPrimary));
+                                        notifyDataSetChanged();
+                                        snackbar1.show();
+                                    }
+                                }).setActionTextColor(context.getResources().getColor(R.color.colorPrimary));
+
+                        context.deleteItemFromCart(object.getItemId(),object.getSku());*/
+
+
+                        //notifyDataSetChanged();
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+                if (holder.swipeLayout != null) {
+                    holder.swipeLayout.close();
+                }
+            }
+        });
     }
 
     @Override
@@ -74,12 +153,14 @@ public class MyPGListRAdapter extends RecyclerSwipeAdapter<MyPGListRAdapter.MyVi
 
     @Override
     public int getSwipeLayoutResourceId(int position) {
-        return 0;
+        return R.id.swipeLayout;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         private final ImageView image;
         private final TextView name, price, date, city;
+        private final SwipeLayout swipeLayout;
+        private final LinearLayout lineLayDelete;
 
         public MyViewHolder(View itemView) {
             super(itemView);
@@ -88,6 +169,9 @@ public class MyPGListRAdapter extends RecyclerSwipeAdapter<MyPGListRAdapter.MyVi
             price = (TextView) itemView.findViewById(R.id.price_pglist);
             date = (TextView) itemView.findViewById(R.id.date_pglist);
             city = (TextView) itemView.findViewById(R.id.city_pglist);
+            swipeLayout = (SwipeLayout)itemView.findViewById(R.id.swipeLayout);
+            lineLayDelete = (LinearLayout)itemView.findViewById(R.id.lineLay_delete);
+
 
         }
     }
